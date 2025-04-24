48873b4f4885dd272cae131878fd5252f512e260
Jordi Cerdà-Bautista <92743001+jordicbau@users.noreply.github.com>
2025-04-24 20:47:30 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('collapsed');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".sidebar-nav li a");
+  const currentPath = window.location.pathname.split("/").pop();
+
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute("href").split("/").pop();
+    if (linkPath === currentPath || (linkPath === "index.html" && currentPath === "")) {
+      link.classList.add("active");
+    }
+  });
+});
